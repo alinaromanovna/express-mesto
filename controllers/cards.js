943ec6ffa77ff099ca1567d4dc0d500bde72430e
Alina <alina-13@bk.ru>
2021-12-17 14:04:03 +0300

    правка ошибок в catch

--- controllers/cards.js
+++ controllers/cards.js
@@ -10,6 +10,7 @@ module.exports.getCards = (req, res) => {
     });
 };
 
+// eslint-disable-next-line consistent-return
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
@@ -17,48 +18,56 @@ module.exports.createCard = (req, res) => {
   if (!name || !link) {
     return res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' });
   }
-  return Card.create({ name, link, owner })
+  Card.create({ name, link, owner })
     .then((card) => {
       res.status(201).send({ data: card });
     })
-    .catch(() => {
-      res.status(400).send({ message: 'Ошибка по умолчанию.' });
+    // eslint-disable-next-line consistent-return
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' });
+      }
+      res.status(500).send({ message: 'Ошибка по умолчанию.' });
     });
 };
 
 module.exports.deleteCard = (req, res) => {
   const { cardId } = req.params;
-
   Card.findByIdAndRemove(cardId)
     // eslint-disable-next-line consistent-return
-    .then(() => {
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+      }
       res.status(200).send({ message: 'Карточка удалена' });
     })
     // eslint-disable-next-line consistent-return
-    .catch(() => {
-      if ('NotFoundError') {
-        return res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные' });
       }
       res.status(500).send({ message: 'Ошибка по умолчанию.' });
     });
 };
+
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-    .then(() => {
-      if (!req.params.cardId) {
+    // eslint-disable-next-line consistent-return
+    .then((card) => {
+      if (!card) {
         return res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
       }
-      return res.status(200).send({ message: 'Лайк поставлен!' });
+      res.status(200).send({ data: card });
     })
-    .catch(() => {
-      if ('CastError') {
-        return res.status(400).send({ message: '400 — Переданы некорректные данные для постановки/снятии лайка.' });
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка.' });
       }
-      return res.status(500).send({ message: '500 — Ошибка по умолчанию.' });
+      return res.status(500).send({ message: 'Ошибка по умолчанию.' });
     });
 };
 
@@ -68,16 +77,18 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true },
   )
+    // eslint-disable-next-line consistent-return
     .then((card) => {
       if (!card) {
         return res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
       }
-      return res.status(200).send({ message: 'Лайк снят!' });
+      res.status(200).send({ data: card });
     })
-    .catch(() => {
-      if ('CastError') {
-        return res.status(400).send({ message: '400 — Переданы некорректные данные для постановки/снятии лайка.' });
+    // eslint-disable-next-line consistent-return
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка.' });
       }
-      return res.status(500).send({ message: '500 — Ошибка по умолчанию.' });
+      res.status(500).send({ message: 'Ошибка по умолчанию.' });
     });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,5 @@
+/* eslint-disable brace-style */
+/* eslint-disable consistent-return */
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
@@ -13,12 +15,16 @@ module.exports.getUsers = (req, res) => {
 module.exports.getUserById = (req, res) => {
   User.findById(req.user._id)
     .then((user) => {
-      if (user) {
-        res.status(200).send({ data: user });
+      if (!user) {
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
       }
-      res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+      res.status(200).send({ data: user });
     })
-    .catch(() => {
+    // eslint-disable-next-line consistent-return
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные' });
+      }
       res.status(500).send({ message: 'Ошибка по умолчанию.' });
     });
 };
@@ -26,16 +32,24 @@ module.exports.getUserById = (req, res) => {
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then(() => {
-      res.status(201).send({ name, about, avatar });
+    .then((user) => {
+      res.status(201).send({ data: user });
     })
-    .catch(() => {
-      res.status(400).send({ message: 'переданы некорректные данные в методы создания карточки;' });
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные в методы создания пользователя;' });
+      }
+      res.status(500).send({ message: 'Ошибка по умолчанию.' });
     });
 };
 
+// eslint-disable-next-line consistent-return
 module.exports.updateUserById = (req, res) => {
   const { name, about } = req.body;
+
+  if (!name || !about) {
+    return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+  }
   User.findByIdAndUpdate(
     req.user._id,
     { name, about },
@@ -44,21 +58,29 @@ module.exports.updateUserById = (req, res) => {
     // eslint-disable-next-line consistent-return
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
       }
-      res.status(200).send({ name, about });
+      res.status(200).send({ data: user });
     })
-    .catch(() => {
-      if ('ValidationError') {
-        return res.status(400).send({ message: 'переданы некорректные данные в методы обновления профиля;' });
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Введены некорректные данные в методы обновления профиля;' });
       }
-      return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+      // eslint-disable-next-line no-else-return
+      else if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+      }
+      res.status(500).send({ message: 'Ошибка по умолчанию.' });
     });
 };
 
+// eslint-disable-next-line consistent-return
 module.exports.updateAvatarById = (req, res) => {
   const { avatar } = req.body;
 
+  if (!avatar) {
+    return res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' });
+  }
   User.findByIdAndUpdate(
     req.user._id,
     { avatar },
@@ -67,13 +89,17 @@ module.exports.updateAvatarById = (req, res) => {
     // eslint-disable-next-line consistent-return
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
       }
-      res.status(200).send({ avatar });
+      res.status(200).send({ data: user });
     })
-    .catch(() => {
-      if ('ValidationError') {
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
         return res.status(400).send({ message: 'переданы некорректные данные в методы создания аватара пользователя;' });
+      }
+      // eslint-disable-next-line no-else-return
+      else if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
       }
       return res.status(500).send({ message: 'Ошибка по умолчанию.' });
     });
